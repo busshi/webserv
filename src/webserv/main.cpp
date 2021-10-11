c85cf03a9ba5ce1d8484c3d3e79f522f8be7706c
abrabant <abrabant@student.42.fr>
2021-10-11 22:29:51 +0200

    Add format rule to automatically format the codebase using clang-format

--- src/webserv/main.cpp
+++ src/webserv/main.cpp
@@ -1,25 +1,25 @@
-#include "Server.hpp"
 #include <iostream>
 
-int		main( int ac, char **av ) {
+#include "Server.hpp"
 
-	if (ac != 2) {
+int
+main(int ac, char** av)
+{
+    if (ac != 2) {
+        // std::cout << "Usage: ./webserv [path to config file]" << std::endl;
+        std::cout << "Usage: ./webserv [port to bind]" << std::endl;
+        return 0;
+    }
 
-		//std::cout << "Usage: ./webserv [path to config file]" << std::endl;
-		std::cout << "Usage: ./webserv [port to bind]" << std::endl;
-		return 0;
-	}
+    Server server;
 
-	Server	server;
+    try {
+        server.init(av[1]);
+        server.start();
+        server.stop();
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
 
-	try {
-		server.init(av[1]);
-		server.start();
-		server.stop();
-	}
-	catch (std::exception & e) {
-		std::cout << e.what() << std::endl;
-	}
-
-	return 0;
+    return 0;
 }
