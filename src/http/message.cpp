12aed218c74be998168b26fba9425ec1b291664b
abrabant <abrabant@student.42.fr>
2021-11-19 00:12:11 +0100

    Add documentation for HTTP messages

--- src/http/message.cpp
+++ src/http/message.cpp
@@ -7,6 +7,13 @@
 
 /* HTTP::Message */
 
+/**
+ * @brief retrieve the value of a given header field.
+ * 
+ * @param name The name of the field, which is case *insensitive*
+ * @return std::string 
+ */
+
 std::string HTTP::Message::getHeaderField(const std::string& name) const
 {
     std::map<std::string, std::string>::const_iterator cit =_header.find(name);
@@ -14,23 +21,53 @@ std::string HTTP::Message::getHeaderField(const std::string& name) const
     return cit != _header.end() ? cit->second : "";
 }
 
+/**
+ * @brief Set the value of a given header field.
+ * 
+ * @param name The name of the field, which is case *insensitive*
+ * @param value The value to assign to the field. If the field already has a value, it gets replaced by the new one.
+ */
+
 void HTTP::Message::setHeaderField(const std::string& name, const std::string& value)
 {
     _header[name] = value;
 }
 
+/**
+ * @brief Construct a new HTTP::Message::Message object
+ * 
+ */
+
 HTTP::Message::Message(void)
 {
 }
+
+/**
+ * @brief Destroy the HTTP::Message::Message object
+ * 
+ */
 
 HTTP::Message::~Message(void)
 {
 }
 
+/**
+ * @brief Construct a new HTTP::Message::Message object
+ * 
+ * @param other
+ */
+
 HTTP::Message::Message(const Message& other)
 {
     *this = other;
 }
+
+/**
+ * @brief Copy data of rhs into lhs
+ * 
+ * @param rhs 
+ * @return HTTP::Message& 
+ */
 
 HTTP::Message& HTTP::Message::operator=(const Message& rhs)
 {
@@ -41,7 +78,12 @@ HTTP::Message& HTTP::Message::operator=(const Message& rhs)
     return *this; 
 }
 
-/* HTTP::Request */
+/**
+ * @brief Construct a new HTTP::Request::Request object
+ * 
+ * @param rawData Basically a stream of bytes, here passed as a std::string. 
+ It is usually the data sent by the user agent through the client socket.
+ */
 
 HTTP::Request::Request(std::string rawData)
 {
@@ -65,15 +107,32 @@ HTTP::Request::Request(std::string rawData)
     _body = rawData.substr(bodyPos + 4, rawData.size() - bodyPos - 4);
     _URI = "http://" + getHeaderField("Host") + _resourceURI;
 }
+/**
+ * @brief Destroy the HTTP::Request::Request object
+ * 
+ */
 
 HTTP::Request::~Request(void)
 {
 }
 
+/**
+ * @brief Construct a new HTTP::Request::Request object
+ * 
+ * @param other 
+ */
+
 HTTP::Request::Request(const HTTP::Request& other): Message()
 {
     *this = other;
 }
+
+/**
+ * @brief Copy data of rhs to lhs
+ * 
+ * @param rhs 
+ * @return HTTP::Request& 
+ */
 
 HTTP::Request& HTTP::Request::operator=(const HTTP::Request& rhs)
 {
@@ -81,20 +140,47 @@ HTTP::Request& HTTP::Request::operator=(const HTTP::Request& rhs)
     return *this;
 }
 
+/**
+ * @brief Get the HTTP verb that corresponds to the request's method as a string
+ * 
+ * @return const std::string& 
+ */
+
 const std::string& HTTP::Request::getMethod(void) const
 {
     return _method;
 }
+
+/**
+ * @brief Get the ressourceURI, i.e the original URI without the protocol and host information.
+ To get the whole URI @see HTTP::Request::getURI.
+ As an example, the ressourceURI of http://aurelienbrabant.fr/content/style.css is /content/style.css
+ * 
+ * @return const std::string& 
+ */
 
 const std::string& HTTP::Request::getResourceURI(void) const
 {
     return _resourceURI;
 }
 
+/**
+ * @brief Get the full URI of the request as typed in the browser's bar.
+ * 
+ * @return const std::string& 
+ */
+
 const std::string& HTTP::Request::getURI(void) const
 {
     return _URI;
 }
+
+/**
+ * @brief Get the protocol used and its version, under the form of <PROTOCOL>/<VERSION>. Given this project's purpose
+ it should always be HTTP/1.1 for HTTP protocol version 1.1.
+ * 
+ * @return const std::string& 
+ */
 
 
 const std::string& HTTP::Request::getProtocol(void) const
@@ -102,30 +188,44 @@ const std::string& HTTP::Request::getProtocol(void) const
     return _protocol;
 }
 
+/**
+ * @brief Get the body of the request. If no body is provided, the empty string is returned.
+ * 
+ * @return const std::string& 
+ */
+
 const std::string& HTTP::Request::getBody(void) const
 {
     return _body;
 }
 
-std::ostream& HTTP::Request::printHeaders(std::ostream& os)
-{
-    for (std::map<std::string, std::string>::const_iterator cit = _header.begin(); cit != _header.end(); ++cit) {
-        os << cit->first << " " << cit->second << "\n";
-    }
-
-    return os;
-}
-
-/* Response */
+/**
+ * @brief Construct a new HTTP::Response::Response object
+ * 
+ * @param req 
+ */
 
 HTTP::Response::Response(const HTTP::Request& req): _statusCode(HTTP::OK), _req(req)
 {
 }
 
+/**
+ * @brief Construct a new HTTP::Response::Response object
+ * 
+ * @param other 
+ */
+
 HTTP::Response::Response(const HTTP::Response& other): Message()
 {
     *this = other;
 }
+
+/**
+ * @brief Assign data of rhs to lhs
+ * 
+ * @param rhs 
+ * @return HTTP::Response& 
+ */
 
 HTTP::Response& HTTP::Response::operator=(const HTTP::Response& rhs)
 {
@@ -136,14 +236,32 @@ HTTP::Response& HTTP::Response::operator=(const HTTP::Response& rhs)
     return *this;
 }
 
+/**
+ * @brief Destroy the HTTP::Response::Response object
+ * 
+ */
+
 HTTP::Response::~Response(void) 
 {
 }
+
+/**
+ * @brief Get the string representation of the response. The returned string can be directly sent back to the user agent.
+ * 
+ * @return std::string 
+ */
 
 std::string HTTP::Response::str(void) const
 {
     return _sendHeader() + _body;
 }
+
+/**
+ * @brief Set the status code of the response using the HTTP::StatusCode enum.
+ * 
+ * @param statusCode 
+ * @return HTTP::Response& 
+ */
 
 HTTP::Response& HTTP::Response::setStatus(StatusCode statusCode)
 {
@@ -152,6 +270,13 @@ HTTP::Response& HTTP::Response::setStatus(StatusCode statusCode)
     return *this;
 }
 
+/**
+ * @brief Set the status code of the response using the value of the HTTP status code.
+ * 
+ * @param intStatusCode 
+ * @return HTTP::Response& 
+ */
+
 HTTP::Response& HTTP::Response::setStatus(unsigned intStatusCode)
 {
     _statusCode = static_cast<StatusCode>(intStatusCode);
@@ -159,10 +284,22 @@ HTTP::Response& HTTP::Response::setStatus(unsigned intStatusCode)
     return *this;
 }
 
+/**
+ * @brief Get the original request the response is answering too.
+ * 
+ * @return const HTTP::Request& 
+ */
+
 const HTTP::Request& HTTP::Response::getReq(void) const
 {
     return _req;
 }
+
+/**
+ * @brief Transform the map of header fields into an actually usuable HTTP header.
+ * 
+ * @return std::string 
+ */
 
 std::string HTTP::Response::_sendHeader(void) const
 {
@@ -177,6 +314,14 @@ std::string HTTP::Response::_sendHeader(void) const
 
     return oss.str();
 }
+
+/**
+ * @brief Send a file as the response's body, automatically figuring out its media type.
+ Use of this member function will overwrite any previously set response's body.
+ * 
+ * @param filepath 
+ * @return HTTP::Response& 
+ */
 
 HTTP::Response& HTTP::Response::sendFile(const std::string& filepath)
 {
@@ -195,6 +340,14 @@ HTTP::Response& HTTP::Response::sendFile(const std::string& filepath)
     return *this;
 }
 
+/**
+ * @brief Send a string as the response's body. Media type text/plain is assumed.
+ Use of this member function will overwrite any previously set response's body.
+ *
+ * @param s 
+ * @return HTTP::Response& 
+ */
+
 HTTP::Response& HTTP::Response::send(const std::string& s)
 {
     _body = s;
@@ -202,12 +355,26 @@ HTTP::Response& HTTP::Response::send(const std::string& s)
     return *this;
 }
 
+/**
+ * @brief Append a string to the response's body, without overwriting anything.
+ * 
+ * @param s 
+ * @return HTTP::Response& 
+ */
+
 HTTP::Response& HTTP::Response::append(const std::string& s)
 {
     _body += s;
     
     return *this;
 }
+
+/**
+ * @brief Easy and modular mediaType detection given the resource file extension (if any)
+ * 
+ * @param resource 
+ * @return std::string 
+ */
 
 std::string HTTP::Response::_detectMediaType(const std::string &resource) const
 {
