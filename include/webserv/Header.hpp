2165bf0167d53a2ae20cb3bf7f1d045affe71539
aldubar <aldubar@student.42.fr>
2021-11-18 15:59:55 +0100

    handle all directives... todo: more check

--- include/webserv/Header.hpp
+++ include/webserv/Header.hpp
@@ -21,27 +21,33 @@ class	Header {
 		void		createResponse( ConfigItem * item );
 
 		struct Direc {
-			std::string	root;
-			std::string 	path;
-			std::string	autoindex;
+			std::string					root;
+			std::string 				path;
+			std::string					autoindex;
 			std::vector<std::string>	indexes;
+			std::vector<std::string>	methods;
+			std::string					uploadPath;
+			std::string					defaultErrorFile;
+			std::string					uploadMaxSize;
+			std::string					bodyMaxSize;
 		};
 
 private:
-		void				_parseFirstLine( std::string, std::string );
-		void				_parseFirstLine( std::string );
-		std::string			_setParam( std::string ) ;
-		std::string			_setContentType( std::string );
-		std::string			_getDate( time_t timestamp );
-		std::string			_getLastModified( std::string path );
-		std::string			_replace( std::string in, std::string s1, std::string s2 );
-		std::string			_getIndex( std::string path, std::vector<std::string> indexes );
-		void        		_genErrorPage( std::string file, std::string code, std::string msg, std::string sentence );
-		void			   	_noAutoIndexResponse( std::string path, std::stringstream & buf );
-		void				_autoIndexResponse( std::string path, std::stringstream & buf );
-		bool        		_isFolder( std::string path );
-		bool        		_haveLocation( std::string requestPath, std::string location);
+		void			_parseFirstLine( std::string, std::string );
+		void			_parseFirstLine( std::string );
+		std::string		_setParam( std::string ) ;
+		std::string		_setContentType( std::string );
+		std::string		_getDate( time_t timestamp );
+		std::string		_getLastModified( std::string path );
+		std::string		_replace( std::string in, std::string s1, std::string s2 );
+		std::string		_getIndex( std::string path, std::vector<std::string> indexes );
+		void       		_genErrorPage( std::string file, std::string code, std::string msg, std::string sentence );
+		void		   	_noAutoIndexResponse( std::string path, std::stringstream & buf );
+		void			_autoIndexResponse( std::string path, std::stringstream & buf );
+		bool       		_isFolder( std::string path );
+		bool       		_haveLocation( std::string requestPath, std::string location);
 		Header::Direc	_getDirectives(ConfigItem * item, std::string suffix);
+		std::string		_checkErrorPage(std::string defaultPage, std::string code, std::string errorMsg, std::string errorSentence );
 
 		std::map<std::string, std::string>	_headerParam;
 		std::string	_response;
